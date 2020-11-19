90186d1a5fe35e6fa9574c97a021b872ed1c17f0
Ralph Plumley <ralphplumley@gmail.com>
2020-11-18 19:49:43 -0500

    Remove console logs. Update readme.

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,8 +1,6 @@
 import React from "react"
 import CONSTANTS from "../constants"
 
-
-
 class Counter extends React.Component {
   constructor(props) {
     super(props)
@@ -10,24 +8,27 @@ class Counter extends React.Component {
   }
 
   checkForCookie() {
-    if (document.cookie.split(';').some((item) => item.trim().startsWith('plumCookie='))) {
+    if (
+      document.cookie
+        .split(";")
+        .some(item => item.trim().startsWith("plumCookie="))
+    ) {
       return true
     } else {
-      let now = new Date();
+      let now = new Date()
       // expire in an hour
-      let time = now.getTime();
-      time += 3600 * 1000; 
+      let time = now.getTime()
+      time += 3600 * 1000
       now.setTime(time)
       document.cookie = "plumCookie=purple; expires=" + now.toUTCString() + ";"
       return false
     }
   }
-  
+
   componentDidMount() {
     const cookie = this.checkForCookie()
-    console.log("cookie state: ", cookie)
 
-    let url;
+    let url
     if (!cookie) {
       // if there's no cookie, we'll consider the user unique
       // so increment
@@ -35,18 +36,16 @@ class Counter extends React.Component {
     } else {
       // get count, but don't update
       url = CONSTANTS.getCountUrl
-      // url = CONSTANTS.incrementCountUrl
     }
 
-    console.log("url: ", url)
     fetch(url)
       .then(response => response.json())
       .then(data => {
-          const count = (cookie ? data : data.data.Attributes.count.N)
-          this.setState({
-            count
-          })
-      });
+        const count = cookie ? data : data.data.Attributes.count.N
+        this.setState({
+          count,
+        })
+      })
   }
 
   render() {
