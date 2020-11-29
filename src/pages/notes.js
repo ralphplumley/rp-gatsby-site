50593f15a74178e8a5ca2ce64bcfd010235b9967
Ralph Plumley <ralphplumley@gmail.com>
2020-11-28 21:17:10 -0500

    create notes.js

---
+++ src/pages/notes.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default function Notes() {
+  return (
+    <Layout>
+      <h3>Notes main page</h3>
+      <p>For Feynman technique / notes to my self</p>
+    </Layout>
+  )
+}
